6828d90c7408242f73a57941946751e9dd0b0b3f
Gilsabo <gilsala_88@hotmail.com>
2023-09-26 18:32:11 +0200

    Remove and move h1 and p to layout

--- app/page.js
+++ app/page.js
@@ -2,10 +2,5 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return (
-    <>
-      <h1 className={styles.title}>Skartista</h1>
-      <p>Grphic designer and artist</p>
-    </>
-  );
+  return <h1 className={styles.title}>home</h1>;
 }
