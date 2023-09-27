d3c58073b7fadca5d2e910c3bdd632dc86fe63b6
Gilsabo <gilsala_88@hotmail.com>
2023-09-27 19:26:28 +0200

    Add dynamic page

---
+++ app/wildLife/[wildLifeId]/page.js
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import { getWildLifeAnimal } from '../../../database/wildLife';
+
+export function generateMetadata({ params }) {
+  const singleAnimal = getWildLifeAnimal(Number(params.wildLifeId));
+
+  return {
+    title: singleAnimal ? singleAnimal.firstName : '',
+  };
+}
+
+export default function WildLifeAnimal(props) {
+  const wildLifeAnimalFromObject = getWildLifeAnimal(
+    Number(props.params.wildLifeId),
+  );
+  console.log(wildLifeAnimalFromObject);
+  console.log(props);
+
+  if (!wildLifeAnimalFromObject) {
+    return notFound();
+  }
+  return (
+    <div>
+      <div>
+        <h1>{wildLifeAnimalFromObject.name}</h1>
+        <Image
+          data-test-id="product-image"
+          src={`/images/${wildLifeAnimalFromObject.name}.jpg`}
+          width={400}
+          height={500}
+          alt={wildLifeAnimalFromObject.name}
+        />
+      </div>
+      <div>
+        <div data-test-id="product-price">{wildLifeAnimalFromObject.name}</div>
+        <div data-test-id="product-price">
+          small size : {wildLifeAnimalFromObject.size[0].small}
+        </div>
+        <div data-test-id="product-price">
+          medium size :{wildLifeAnimalFromObject.size[0].medium}
+        </div>
+        <div data-test-id="product-price">
+          large size : {wildLifeAnimalFromObject.size[0].large}
+        </div>
+      </div>
+    </div>
+  );
+}
