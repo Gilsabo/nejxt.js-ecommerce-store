22593491ff2f0eb363988dc9930c5d12c87fdfe5
Gilsabo <gilsala_88@hotmail.com>
2023-09-27 16:05:52 +0200

    Add links to each image in about section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,9 @@
 import Image from 'next/image';
+import baby3 from '../public/images/baby-3.jpg';
+import cat from '../public/images/cat-black-white.jpg';
+import cowSeated from '../public/images/cow-seated.jpg';
+import landscapeMoon from '../public/images/landscape-moon.jpg';
+import wedding from '../public/images/wedding.jpg';
 import styles from './page.module.css';
 
 export const metadata = {
@@ -6,5 +11,29 @@ export const metadata = {
 };
 
 export default function Home() {
-  return <h1 className={styles.title}>home</h1>;
+  return (
+    <div>
+      <h1 className={styles.title}>home</h1>
+      <a href="/landscapes" data-test-id="product-1">
+        <Image src={landscapeMoon} alt="landscape" width={200} height={250} />
+        <div>Landscapes</div>
+      </a>
+      <a href="/wildLife" data-test-id="product-1">
+        <Image src={cowSeated} alt="cow" width={200} height={250} />
+        <div>Wild Life</div>
+      </a>
+      <a href="/family" data-test-id="product-1">
+        <Image src={baby3} alt="baby" width={200} height={250} />
+        <div>Family</div>
+      </a>
+      <a href="/pets" data-test-id="product-1">
+        <Image src={cat} alt="cat" width={200} height={250} />
+        <div>Pets</div>
+      </a>
+      <a href="/events" data-test-id="product-1">
+        <Image src={wedding} alt="wedding" width={200} height={250} />
+        <div>Events</div>
+      </a>
+    </div>
+  );
 }
