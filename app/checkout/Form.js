5ce60805cca406952f13bbc4e69e57bdc3f223fe
Gilsabo <gilsala_88@hotmail.com>
2023-10-03 07:37:17 +0200

    Create form for checkout

---
+++ app/checkout/Form.js
@@ -0,0 +1,42 @@
+'use client';
+
+export default function Form() {
+  return (
+    <fom>
+      <label type="text">
+        First name <input data-test-id="checkout-first-name" required />
+      </label>
+      <label type="text">
+        Last name <input data-test-id="checkout-last-name" required />
+      </label>
+      <label type="email">
+        E-mail <input data-test-id="checkout-email" required />
+      </label>
+      <label type="adress">
+        Adress <input data-test-id="checkout-address" required />
+      </label>
+      <label type="text">
+        Adress <input data-test-id="checkout-address" required />
+      </label>
+      <label type="text">
+        City <input data-test-id="checkout-city" required />
+      </label>
+      <label type="text">
+        Postal code <input data-test-id="checkout-postal-code" required />
+      </label>
+      <label type="number">
+        Postal code <input data-test-id="checkout-country" required />
+      </label>
+      <label type="number">
+        Credit card <input data-test-id="checkout-credit-card" />
+      </label>
+      <label type="number">
+        Expiration date <input data-test-id="checkout-credit-card" required />
+      </label>
+      <label type="number">
+        Security code <input data-test-id="checkout-security-code" required />
+      </label>
+      <button data-test-id="checkout-confirm-order">Confirm order </button>
+    </fom>
+  );
+}
