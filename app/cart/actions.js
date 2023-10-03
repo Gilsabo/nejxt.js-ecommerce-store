20605dee93947ecee1465ec682a49d44cf8204b3
Gilsabo <gilsala_88@hotmail.com>
2023-10-03 15:20:11 +0200

    Remove console.log with await

--- app/cart/actions.js
+++ app/cart/actions.js
@@ -8,7 +8,7 @@ export default async function RemoveButtonFunction(wildLifeWithQuantityId) {
 
   const wildLifeCookie = getCookie('wildLifePaintings');
   console.log('id', wildLifeWithQuantityId);
-  await console.log('cookie', wildLifeCookie);
+  console.log('cookie', wildLifeCookie);
 
   const wildLifeAnimalsInCart = !wildLifeCookie
     ? // Case A: cookie is undefined
@@ -21,28 +21,9 @@ export default async function RemoveButtonFunction(wildLifeWithQuantityId) {
     (wildLifeAnimalInCart) =>
       wildLifeAnimalInCart.id !== wildLifeWithQuantityId,
   );
+
   console.log('stays', wildLifeItemsRemoved);
 
-  // 3. we edit the cookie value
-
-  // We get the the object for the fruit on cookies or undefined
-  // const wildLifeQuantityToUpdate = wildLifeQuantities.find(
-  //   (wildLifeQuantity) => {
-  //     return wildLifeQuantity.id === wildLifeId;
-  //   },
-  // );
-  // Case B: cookie is defined and fruit id already exists!
-  // if we are in fruit id = 1
-  // if (wildLifeQuantityToUpdate) {
-  //   wildLifeQuantityToUpdate.quantity = quantity;
-  // } else {
-  //   // Case C: cookie is defined and fruit id doesn't exist!
-  //   wildLifeQuantities.push({
-  //     id: wildLifeId,
-  //     quantity: quantity,
-  //   });
-
-  // 4. we override the cookie
   await cookies().set(
     'wildLifePaintings',
     JSON.stringify(wildLifeItemsRemoved),
