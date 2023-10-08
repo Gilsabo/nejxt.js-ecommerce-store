d93710c0e01df7515776e477583b2b01d0d5fe41
Gilsabo <gilsala_88@hotmail.com>
2023-10-08 14:56:43 +0200

    Change file to tsx

--- app/cart/actions.tsx
+++ app/cart/actions.tsx
@@ -3,9 +3,15 @@ import { cookies } from 'next/headers';
 import { getCookie } from '../../util/cookies';
 import { parseJson } from '../../util/json';
 
-export default async function RemoveButtonFunction(wildLifeWithQuantityId) {
+export default async function RemoveButtonFunction(
+  wildLifeWithQuantityId: number,
+) {
   // 1. get the current cookie
 
+  type WildLifeAnimalsInCart = {
+    id: number;
+    quantity: number;
+  };
   const wildLifeCookie = getCookie('cart');
   console.log('id', wildLifeWithQuantityId);
   console.log('cookie', wildLifeCookie);
@@ -18,7 +24,7 @@ export default async function RemoveButtonFunction(wildLifeWithQuantityId) {
 
   const wildLifeItemsRemoved = wildLifeAnimalsInCart.filter(
     // this is not really removing, is taking the one you do not click
-    (wildLifeAnimalInCart) =>
+    (wildLifeAnimalInCart: WildLifeAnimalsInCart) =>
       wildLifeAnimalInCart.id !== wildLifeWithQuantityId,
   );
 
