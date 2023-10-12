e422cc02f378579654c5e7e2384421e730519581
Gilsabo <gilsala_88@hotmail.com>
2023-10-12 07:35:17 +0200

    Play around with svg to align it

--- app/layout.js
+++ app/layout.js
@@ -39,11 +39,25 @@ export default function RootLayout({ children }) {
             <Link href="/landscapes">Landscapes</Link>
             <Link href="/pets">Pets</Link>
             <Link href="/wildLife">Wild life</Link>
-            <Link href="/cart">Shopping cart</Link>
+            {/* <Link href="/cart">Shopping cart</Link> */}
             <header data-test-id="cart-link">
-              <Link data-test-id="cart-link" href="/cart">
-                Items:
-                {numberOfItems ?? (
+              <Link
+                className={styles.backpackCart}
+                data-test-id="cart-link"
+                href="/cart"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 35 35"
+                  fill="none"
+                >
+                  <path
+                    d="m15.75 11.8h-3.16l-.77 11.6a5 5 0 0 0 4.99 5.34h7.38a5 5 0 0 0 4.99-5.33l-.78-11.61zm0 1h-2.22l-.71 10.67a4 4 0 0 0 3.99 4.27h7.38a4 4 0 0 0 4-4.27l-.72-10.67h-2.22v.63a4.75 4.75 0 1 1 -9.5 0zm8.5 0h-7.5v.63a3.75 3.75 0 1 0 7.5 0z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+                {numberOfItems > 0 && (
                   <span data-test-id="cart-count">{numberOfItems}</span>
                 )}
               </Link>
