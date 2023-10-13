9ac8f22b9806badec46e4ad3175e351e8b1bc491
Gilsabo <gilsala_88@hotmail.com>
2023-10-13 08:01:11 +0200

    Place the the input field whithing label

--- app/checkout/Form.js
+++ app/checkout/Form.js
@@ -1,40 +1,43 @@
 import ClearButton from './ClearButton';
+import styles from './form.module.css';
 
 export default function Form() {
   return (
-    <form>
-      <label type="text">
-        First name <input data-test-id="checkout-first-name" required />
+    <form className={styles.form}>
+      <label>
+        First name
+        <input data-test-id="checkout-first-name" required />
       </label>
-      <label type="text">
-        Last name <input data-test-id="checkout-last-name" required />
+      <label>
+        Last name
+        <input data-test-id="checkout-last-name" required />
       </label>
-      <label type="email">
-        E-mail <input data-test-id="checkout-email" required />
+      <label>
+        E-mail <input type="email" data-test-id="checkout-email" required />
       </label>
-      <label type="adress">
-        Adress <input data-test-id="checkout-address" required />
+      <label>
+        Address <input data-test-id="checkout-address" required />
       </label>
-      <label type="text">
-        Adress <input data-test-id="checkout-address" required />
-      </label>
-      <label type="text">
+      <label>
         City <input data-test-id="checkout-city" required />
       </label>
-      <label type="text">
-        Postal code <input data-test-id="checkout-postal-code" required />
+      <label>
+        Postal code
+        <input type="number" data-test-id="checkout-postal-code" required />
       </label>
-      <label type="number">
-        Postal code <input data-test-id="checkout-country" required />
+      <label>
+        Country <input data-test-id="checkout-country" required />
       </label>
-      <label type="number">
-        Credit card <input data-test-id="checkout-credit-card" />
+      <label>
+        Credit card <input type="number" data-test-id="checkout-credit-card" />
       </label>
-      <label type="number">
-        Expiration date <input data-test-id="checkout-credit-card" required />
+      <label>
+        Expiration date
+        <input data-test-id="checkout-expiration-date" required />
       </label>
-      <label type="number">
-        Security code <input data-test-id="checkout-security-code" required />
+      <label>
+        Security code
+        <input type="number" data-test-id="checkout-security-code" required />
       </label>
       <ClearButton />
     </form>
