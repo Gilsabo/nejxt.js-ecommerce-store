735e5305d0a8347f06d44868169567d6df59fa9e
Gilsabo <gilsala_88@hotmail.com>
2023-10-13 15:02:33 +0200

    Style for José farwell

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -9,6 +9,12 @@ export default function ThankYouPage() {
     <div className={styles.mainContainer}>
       <h1>Thank you for your order</h1>
       <div className={styles.heart}>❤️</div>
+      <h1>Getting closer to pay my bills</h1>
+      <div className={styles.heart}>💸</div>
+      <div>
+        <h1>Gracias por todo Jose, good energy!</h1>
+        <div className={styles.heart}>👍👍👍👍👍</div>
+      </div>
     </div>
   );
 }
