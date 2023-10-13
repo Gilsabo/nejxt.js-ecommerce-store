1c4808bd2df72faad1bca30002617d0bf3727e60
Gilsabo <gilsala_88@hotmail.com>
2023-10-13 08:46:30 +0200

    Add heart emoji

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -1,7 +1,14 @@
+import styles from './thankYouPage.module.css';
+
 export const metadata = {
   title: 'Thank you for your order',
 };
 
 export default function ThankYouPage() {
-  return <div>Thank you for your order</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <h1>Thank you for your order</h1>
+      <div className={styles.heart}>❤️</div>
+    </div>
+  );
 }
