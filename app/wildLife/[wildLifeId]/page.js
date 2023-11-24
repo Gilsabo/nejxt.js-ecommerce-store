6b9e9b83acebb5f41243dc3b540557c640426c7e
Gilsabo <gilsala_88@hotmail.com>
2023-11-24 16:40:37 +0100

    Remove unnecessary comments

--- app/wildLife/[wildLifeId]/page.js
+++ app/wildLife/[wildLifeId]/page.js
@@ -1,8 +1,6 @@
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import { getWildLifeAnimal } from '../../../database/wildLife';
-// import { getCookie } from '../../../util/cookies';
-// import { parseJson } from '../../../util/json';
 import ChangeQuantityButton from './ChangeQuantityButton';
 import styles from './wildLIfePageDynamic.module.css';
 
@@ -22,15 +20,6 @@ export default async function WildLifeAnimal(props) {
   if (!wildLifeAnimalFromObject) {
     return notFound();
   }
-
-  // I DO NOT KNOW IF I NEED THIS CODE. WITHOUT IT WORKS FINE UNTIL NOW
-  // const wildLifeCookie = getCookie('wildLifePaintings');
-
-  // const wildLifeQuantities = !wildLifeCookie ? [] : parseJson(wildLifeCookie);
-
-  // const wildLifeToDisplay = wildLifeQuantities.find((wildLifeQuantity) => {
-  // return wildLifeQuantity.id === wildLifeAnimalFromObject.id;
-  // });
 
   const sizesArray = JSON.parse(wildLifeAnimalFromObject.size);
   const firstSizeObject = sizesArray[0];
